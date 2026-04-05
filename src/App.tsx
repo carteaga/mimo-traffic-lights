14dea9ef5bd713ecae8f77ff4a32ed37a939690c
Cristian Arteaga <carteaga19@gmail.com>
2026-04-05 14:32:37 -0400

    chore: bootstrap vite react typescript app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>mimo-traffic-lights</h1>
+      <p>Proyecto inicial listo para desarrollar el simulador educativo.</p>
+    </main>
+  )
+}
+
+export default App
