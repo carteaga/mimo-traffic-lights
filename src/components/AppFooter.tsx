6d0126230a86e537e066800c4a1aebf34be484dd
Cristian Arteaga <carteaga19@gmail.com>
2026-04-05 23:57:24 -0400

    Add GitHub icon to footer repo link

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -2,8 +2,22 @@ export function AppFooter() {
   return (
     <footer className="footer">
       <p>Vibecodeado por Cristian</p>
-      <a href="https://github.com/carteaga/" target="_blank" rel="noreferrer">
-        github.com/carteaga
+      <a
+        className="footer-repo-link"
+        href="https://github.com/carteaga/mimo-traffic-lights"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <svg
+          aria-hidden="true"
+          viewBox="0 0 24 24"
+          width="18"
+          height="18"
+          fill="currentColor"
+        >
+          <path d="M12 .5C5.65.5.5 5.65.5 12c0 5.08 3.29 9.39 7.86 10.9.58.11.79-.25.79-.56 0-.28-.01-1.2-.02-2.18-3.2.7-3.88-1.36-3.88-1.36-.52-1.33-1.28-1.68-1.28-1.68-1.05-.72.08-.7.08-.7 1.16.08 1.78 1.2 1.78 1.2 1.03 1.76 2.69 1.25 3.34.95.1-.75.4-1.25.72-1.54-2.55-.29-5.23-1.27-5.23-5.68 0-1.26.45-2.28 1.19-3.08-.12-.29-.52-1.45.11-3.02 0 0 .97-.31 3.19 1.18a11.1 11.1 0 0 1 5.81 0c2.22-1.49 3.19-1.18 3.19-1.18.63 1.57.23 2.73.11 3.02.74.8 1.19 1.82 1.19 3.08 0 4.42-2.69 5.39-5.25 5.67.41.36.78 1.08.78 2.19 0 1.58-.01 2.85-.01 3.24 0 .31.21.68.8.56A11.5 11.5 0 0 0 23.5 12C23.5 5.65 18.35.5 12 .5Z" />
+        </svg>
+        <span>Repositorio en GitHub</span>
       </a>
     </footer>
   )
